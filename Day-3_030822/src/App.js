bf1c30dcf0c1abd521244675dcf0c32c8a86b3b2
Sourav Saraf <souravsaraf2000@rediffmail.com>
2022-08-04 19:37:27 +0530

    Code

---
+++ Day-3_030822/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Greeting from './greeting';
+
+function App() {
+  return (
+    <div className="App">
+      <Greeting />
+    </div>
+  );
+}
+
+export default App;
---
+++ Day-3_030822/src/greeting.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Greeting = () => {
+    return (
+        <h1 style={{backgroundColor: 'lightblue'}}>Hello From Greeting Component</h1>
+        // React.createElement("h1",{style:{backgroundColor: 'lightblue'}},"Hello From Greeting Component")
+    )
+}
+
+export default Greeting;
